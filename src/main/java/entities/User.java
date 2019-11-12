b4f51a59bfce417ada4a190dc09c9ecb32c429b5
MehdiWertani <wertanimehdi1@gmail.com>
2019-11-12 22:51:34 +0100

    added user entity

---
+++ src/main/java/entities/User.java
@@ -0,0 +1,14 @@
+package entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private Integer idUser;
+}
