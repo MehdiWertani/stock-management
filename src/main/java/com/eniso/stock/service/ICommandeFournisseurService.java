cacf4794a3cf527cb8db3408410daf1ba0b85f9f
MehdiWertani <wertanimehdi1@gmail.com>
2019-11-17 04:28:49 +0100

    commandeFournisseurService

--- src/main/java/com/eniso/stock/service/ICommandeFournisseurService.java
+++ src/main/java/com/eniso/stock/service/ICommandeFournisseurService.java
@@ -1,4 +1,17 @@
 package com.eniso.stock.service;
 
+import com.eniso.stock.entities.CommandeFournisseur;
+
+import java.util.List;
+
 public interface ICommandeFournisseurService {
+    CommandeFournisseur save(CommandeFournisseur commandeFournisseur);
+
+    void delete(CommandeFournisseur commandeFournisseur);
+
+    CommandeFournisseur update(CommandeFournisseur commandeFournisseur);
+
+    CommandeFournisseur getOne(Long id);
+
+    List<CommandeFournisseur> getAll();
 }
