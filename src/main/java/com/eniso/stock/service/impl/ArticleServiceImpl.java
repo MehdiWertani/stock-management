fe1feca9b3a895396cd4171e7013aeb55eeb017c
MehdiWertani <wertanimehdi1@gmail.com>
2019-11-17 03:45:02 +0100

    IarticleService Impl

--- src/main/java/com/eniso/stock/service/impl/ArticleServiceImpl.java
+++ src/main/java/com/eniso/stock/service/impl/ArticleServiceImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
+@Service("ArticleServiceImpl")
 public class ArticleServiceImpl implements IArticleService {
     IArticleDao iArticleDao;
      @Autowired
