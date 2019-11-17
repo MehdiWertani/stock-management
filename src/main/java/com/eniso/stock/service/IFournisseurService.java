303a7a8bc86b03897c72c356fb30c09a7781b49d
MehdiWertani <wertanimehdi1@gmail.com>
2019-11-17 04:43:40 +0100

    IFournisseurService

--- src/main/java/com/eniso/stock/service/IFournisseurService.java
+++ src/main/java/com/eniso/stock/service/IFournisseurService.java
@@ -1,4 +1,17 @@
 package com.eniso.stock.service;
 
+import com.eniso.stock.entities.Fournisseur;
+
+import java.util.List;
+
 public interface IFournisseurService {
+    Fournisseur save(Fournisseur fournisseur);
+
+    void delete(Fournisseur fournisseur);
+
+    Fournisseur update(Fournisseur fournisseur);
+
+    Fournisseur getOne(Long id);
+
+    List<Fournisseur> getAll();
 }
