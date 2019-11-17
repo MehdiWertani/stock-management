e1f8b6efba34bb63cad02054ea685daf8b76a9bf
MehdiWertani <wertanimehdi1@gmail.com>
2019-11-17 05:36:36 +0100

    ILigneCommandeClientService

--- src/main/java/com/eniso/stock/service/ILigneCommandeClientService.java
+++ src/main/java/com/eniso/stock/service/ILigneCommandeClientService.java
@@ -1,4 +1,17 @@
 package com.eniso.stock.service;
 
+import com.eniso.stock.entities.LigneCommandeClient;
+
+import java.util.List;
+
 public interface ILigneCommandeClientService {
+    LigneCommandeClient save(LigneCommandeClient ligneCommandeClient);
+
+    void delete(LigneCommandeClient ligneCommandeClient);
+
+    LigneCommandeClient update(LigneCommandeClient ligneCommandeClient);
+
+    LigneCommandeClient getOne(Long id);
+
+    List<LigneCommandeClient> getAll();
 }
