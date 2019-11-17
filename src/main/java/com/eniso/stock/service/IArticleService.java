d43609bda61b8186cbf194fe0e5128b54fd4526d
MehdiWertani <wertanimehdi1@gmail.com>
2019-11-17 03:29:52 +0100

    IarticleService

--- src/main/java/com/eniso/stock/service/IArticleService.java
+++ src/main/java/com/eniso/stock/service/IArticleService.java
@@ -1,4 +1,17 @@
 package com.eniso.stock.service;
 
+import com.eniso.stock.entities.Article;
+
+import java.util.List;
+
 public interface IArticleService {
+    Article save(Article article);
+
+    void delete(Article article);
+
+    Article update(Article article);
+
+    Article getOne(Long id);
+
+    List<Article> getAll();
 }
