b4a16862dc433f4c12b9b405bc3c324453569ed6
MehdiWertani <wertanimehdi1@gmail.com>
2019-11-17 04:03:56 +0100

    catagoryService

--- src/main/java/com/eniso/stock/service/ICatagoryService.java
+++ src/main/java/com/eniso/stock/service/ICatagoryService.java
@@ -1,4 +1,17 @@
 package com.eniso.stock.service;
 
+import com.eniso.stock.entities.Catagory;
+
+import java.util.List;
+
 public interface ICatagoryService {
+    Catagory save(Catagory catagory);
+
+    void delete(Catagory catagory);
+
+    Catagory update(Catagory catagory);
+
+    Catagory getOne(Long id);
+
+    List<Catagory> getAll();
 }
